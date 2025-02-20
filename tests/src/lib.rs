fdbb4299b0bb6348f545dc92395adb70fe1e0009
SDeanEgan <sdeanegan@gmx.com>
2025-02-20 18:26:06 +0000

    Included additional stuff to tests

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,3 +1,28 @@
+
+/*
+ * COMMAND LINE TESTING ARGUMENTS
+ * use `cargo test` to run the tests normally
+ * or use `cargo test --help` for options with `cargo test`
+ * or use `cargo test -- --help` for options with the separator
+ * 
+ * tests run in multithreaded mode by default, to prevent this:
+ * use `cargo test -- --test-threads=1`
+ * 
+ * passing tests redirect println! output by default, to prevent this:
+ * use `cargo test -- --show-output`
+ * 
+ * run tests by name:
+ * use `cargo test [TEST FUNCTION NAME]`
+ * or a subset of tests based on those whose names contain some part:
+ * use `cargo test [PART OF FUNCTION NAME]` 
+ * 
+ * tests can be ignored with `#[ignore]` above the signature:
+ * use `cargo test -- --ignored` to run only ignored tests
+ * or use `cargo test -- --include-ignored` for all tests
+ * 
+ */
+
+
 #[derive(PartialEq, Debug)]
 struct Rectangle{
 	width: u32,
@@ -37,6 +62,7 @@ mod tests {
 	}
 	
 	#[test]
+	#[ignore]
 	fn failure() {
 		panic!("You just can't win.");
 	}
@@ -70,6 +96,7 @@ mod tests {
 	}
 	
 	#[test]
+	#[ignore]
 	fn test_msg() {
 		let result = subtract(2, 1);
 		assert_ne!(
